fc3d3f1d4e9663f291f4521669d71071630f893d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-06-29 22:03:01 +0800

    commit

--- app/functions.py
+++ app/functions.py
@@ -65,6 +65,132 @@ def getMachinery(machineryCode, key, mode, file_name):
         print("Error: " + str(e) + " (" + key + ": " + machineryCode + ")" + "\n")
 
 
+def generateMainData(file_name):
+    path = "src/" + file_name
+    print("Excel File: " + file_name)
+
+    # Read the data
+    data = pd.read_excel(path, sheet_name=None, index_col=None, header=None)
+
+    # Get the keys
+    xl = pd.ExcelFile(path)
+    keys = xl.sheet_names
+
+    # Iterate through the sheets
+    for key in keys:
+        if key not in notIncluded:
+            print(key)
+
+            # Vessel Name
+            vessel = data[key].iloc[0, 2]
+
+            # Default Machinery Name: machinery = data[key].iloc[2, 2]
+            # Machinery Name using the machinery code
+            machinery = getMachinery(
+                str(data[key].iloc[2, 5]).rstrip(), key, "main", file_name
+            )
+
+            if (not pd.isna(machinery)) and (not pd.isna(vessel)):
+                # Start traversing the data on row 7
+                row = 7
+                isValid = True
+
+                # Prepare the sheets
+                book = Workbook()
+                sheet = book.active
+
+                sheet.append(main_header)
+
+                while isValid:
+
+                    rowData = (
+                        vessel,
+                        machinery,
+                    )
+
+                    for col in range(7):
+                        d = data[key].iloc[row, col]
+
+                        if (pd.isna(d)) and (col == 0):
+                            isValid = False
+                            break
+
+                        if pd.isna(d):
+                            d = " "
+
+                        if (col == 3) and not (re.search("[a-zA-Z]", str(d))):
+                            d = str(d) + " Hours"
+
+                        if ((col == 4) or (col == 5)) and isinstance(d, datetime):
+                            d = d.strftime("%d-%b-%y")
+                        else:
+                            d = re.sub("\\s+", " ", str(d))
+
+                        tempTuple = (d,)
+                        rowData += tempTuple
+
+                    if isValid:
+                        sheet.append(rowData)
+                        row += 1
+
+                create_name = file_name[: len(file_name) - 4]
+                creation_folder = "./res/main/" + create_name
+                if not os.path.exists(creation_folder):
+                    os.makedirs(creation_folder)
+                book.save(creation_folder + "/" + key + ".xlsx")
+
+    print("Done...")
+
+
+def generateSubData(file_name):
+    path = "src/" + file_name
+    print("Excel File: " + file_name)
+
+    # Read the data
+    data = pd.read_excel(path, sheet_name=None, index_col=None, header=None)
+
+    # Get the keys
+    xl = pd.ExcelFile(path)
+    keys = xl.sheet_names
+
+    # Get updated_at
+    updating_date = data["Running Hours"].iloc[2, 3].strftime("%d-%b-%y")
+    # print(updating_date)
+
+    # Prepare the sheets
+    book = Workbook()
+    sheet = book.active
+
+    # Append the dates
+    sheet.append(sub_header)
+
+    # Iterate through the sheets
+    for key in keys:
+        if key not in notIncluded:
+            print(key)
+
+            # Vessel Name
+            vessel = data[key].iloc[0, 2]
+
+            # Default Machinery Name: machinery = data[key].iloc[2, 2]
+            # Machinery Name
+            machinery = getMachinery(str(data[key].iloc[2, 5]), key, "sub", file_name)
+
+            # Running Hours
+            running_hours = data[key].iloc[3, 5]
+
+            rowData = (vessel, machinery, running_hours, updating_date)
+            sheet.append(rowData)
+
+    create_name = file_name[: len(file_name) - 4]
+    creation_folder = "./res/sub/" + create_name
+    if not os.path.exists(creation_folder):
+        os.makedirs(creation_folder)
+    book.save(creation_folder + "/" + file_name)
+
+    print("Done...")
+
+
 def main_function():
     if not os.path.exists("./res/main"):
         os.makedirs("./res/main")
@@ -82,92 +208,23 @@ def main_function():
                 i += 1
 
         if len(files) == 0:
-            print("No such data found in src directory.")
-            exit()
+            print("No data found in src directory.")
+            break
+
+        print("A - All")
 
         # Get the location of the data
         try:
-            file_key = input("\nInput file number: ")
-            file_name = files[int(file_key)]
-            path = "src/" + file_name
-            print("Excel File: " + file_name)
-        except Exception as e:
-            print("Error: ", str(e))
+            file_key = input("\nSelect an option: ")
 
-        # Read the data
-        try:
-            data = pd.read_excel(path, sheet_name=None, index_col=None, header=None)
+            if file_key != "A":
+                file_name = files[int(file_key)]
+                generateMainData(file_name)
 
-            # Get the keys
-            xl = pd.ExcelFile(path)
-            keys = xl.sheet_names
+            else:
+                for _file in files:
+                    generateMainData(_file)
 
-            # Iterate through the sheets
-            for key in keys:
-                if key not in notIncluded:
-                    print(key)
-
-                    # Vessel Name
-                    vessel = data[key].iloc[0, 2]
-
-                    # Default Machinery Name: machinery = data[key].iloc[2, 2]
-                    # Machinery Name using the machinery code
-                    machinery = getMachinery(
-                        str(data[key].iloc[2, 5]).rstrip(), key, "main", file_name
-                    )
-
-                    if (not pd.isna(machinery)) and (not pd.isna(vessel)):
-                        # Start traversing the data on row 7
-                        row = 7
-                        isValid = True
-
-                        # Prepare the sheets
-                        book = Workbook()
-                        sheet = book.active
-
-                        sheet.append(main_header)
-
-                        while isValid:
-
-                            rowData = (
-                                vessel,
-                                machinery,
-                            )
-
-                            for col in range(7):
-                                d = data[key].iloc[row, col]
-
-                                if (pd.isna(d)) and (col == 0):
-                                    isValid = False
-                                    break
-
-                                if pd.isna(d):
-                                    d = " "
-
-                                if (col == 3) and not (re.search("[a-zA-Z]", str(d))):
-                                    d = str(d) + " Hours"
-
-                                if ((col == 4) or (col == 5)) and isinstance(
-                                    d, datetime
-                                ):
-                                    d = d.strftime("%d-%b-%y")
-                                else:
-                                    d = re.sub("\\s+", " ", str(d))
-
-                                tempTuple = (d,)
-                                rowData += tempTuple
-
-                            if isValid:
-                                sheet.append(rowData)
-                                row += 1
-
-                        create_name = file_name[: len(file_name) - 4]
-                        creation_folder = "./res/main/" + create_name
-                        if not os.path.exists(creation_folder):
-                            os.makedirs(creation_folder)
-                        book.save(creation_folder + "/" + key + ".xlsx")
-
-            print("Done...")
         except Exception as e:
             print("Error: " + str(e))
 
@@ -194,63 +251,20 @@ def sub_function():
 
         if len(files) == 0:
             print("No such data found in src directory.")
-            exit()
+            break
 
-        # Get the location of the data
+        print("A - All")
+
         try:
             file_key = input("\nInput file number: ")
-            file_name = files[int(file_key)]
-            path = "src/" + file_name
-            print("Excel File: " + file_name)
-        except Exception as e:
-            print("Error: ", str(e))
 
-        # Read the data
-        try:
-            data = pd.read_excel(path, sheet_name=None, index_col=None, header=None)
+            if file_key != "A":
+                file_name = files[int(file_key)]
+                generateSubData(file_name)
+            else:
+                for _file in files:
+                    generateSubData(_file)
 
-            # Get the keys
-            xl = pd.ExcelFile(path)
-            keys = xl.sheet_names
-
-            # Get updated_at
-            updating_date = data["Running Hours"].iloc[2, 3].strftime("%d-%b-%y")
-            # print(updating_date)
-
-            # Prepare the sheets
-            book = Workbook()
-            sheet = book.active
-
-            # Append the dates
-            sheet.append(sub_header)
-
-            # Iterate through the sheets
-            for key in keys:
-                if key not in notIncluded:
-                    print(key)
-
-                    # Vessel Name
-                    vessel = data[key].iloc[0, 2]
-
-                    # Default Machinery Name: machinery = data[key].iloc[2, 2]
-                    # Machinery Name
-                    machinery = getMachinery(
-                        str(data[key].iloc[2, 5]), key, "sub", file_name
-                    )
-
-                    # Running Hours
-                    running_hours = data[key].iloc[3, 5]
-
-                    rowData = (vessel, machinery, running_hours, updating_date)
-                    sheet.append(rowData)
-
-            create_name = file_name[: len(file_name) - 4]
-            creation_folder = "./res/sub/" + create_name
-            if not os.path.exists(creation_folder):
-                os.makedirs(creation_folder)
-            book.save(creation_folder + "/" + file_name)
-
-            print("Done...")
         except Exception as e:
             print("Error: " + str(e))
 
